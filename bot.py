4185d21d5dfeb6b4fd283bf89ff6d1794af706f3
Arung Agamani <arung.agamani@gmail.com>
2021-11-16 01:21:04 +0700

    Created QA Agent and ElasticSearch agent

---
+++ bot.py
@@ -0,0 +1,36 @@
+import discord
+from qa_agent import QA_Agent
+from es_agent import ElasticSearchAgent
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = discord.Client()
+
+es = ElasticSearchAgent('localhost', '9200', 'genshin')
+print("Initializing QA Agent")
+qa_agent = QA_Agent()
+print("QA Agent initialized")
+
+@client.event
+async def on_ready():
+    print("Logged in")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('$q '):
+        question = message.content[3:]
+        es_result = es.search(question, 3)
+        if len(es_result) > 0:
+            passage = es_result[0]['_source']['content']
+            res = qa_agent.predict(context=passage, question=question)
+            await message.channel.send(res)
+            
+        else:
+            await message.channel.send("No data found on that question. Sorry, Traveler")
+
+client.run(os.environ.get("BOT_TOKEN"))
